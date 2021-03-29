0f194e085ae7be7fa9804cd309a393acbc1bc23f
M. Javad Mohebbi <mohammadjavadmohebbi@gmail.com>
2021-03-29 11:55:44 +0430

    reverse DNS Lookup

--- influxdb/reverseDnsLookup.go
+++ influxdb/reverseDnsLookup.go
@@ -1,11 +1,14 @@
 package influxdb
 
-import "net"
+import (
+	"net"
+)
 
 // get ip addr & try to get the reverse dns lookup
 func (i *InfluxDBv2) revereseDNS(ip string) string {
 	addr, err := net.LookupAddr(ip)
 
+	// if no lookup, concat NA- to ip & return it
 	if err != nil {
 		return "NA-" + ip
 	}
