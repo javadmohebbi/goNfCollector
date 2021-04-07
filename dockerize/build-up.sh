d0750d8a0bb077ca285acee6ecc050413556a89c
M. Javad Mohebbi <mohammadjavadmohebbi@gmail.com>
2021-04-07 17:25:50 +0430

    Update Volum Path

--- dockerize/build-up.sh
+++ dockerize/build-up.sh
@@ -46,6 +46,7 @@ download_latest_version() {
 
     # download grafana dashboards & conf
     wget -O /tmp/grafana.tar.gz https://download.openintelligence24.com/vendors/grafana/grafana.tar.gz && tar -vxf /tmp/grafana.tar.gz -C $GRAFANA_DIR/
+    chmod +r $GRAFANA_DIR/ -Rv
 
     wget -O ./docker-compose.yml https://download.openintelligence24.com/vendors/docker-compose/docker-compose.yml
 
