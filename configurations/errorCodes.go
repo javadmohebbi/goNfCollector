080c8591601864cd5329ac2da8502fff6ac33f9e
M. Javad Mohebbi <mohammadjavadmohebbi@gmail.com>
2021-06-02 11:52:07 +0430

    Update Error Codes

--- configurations/errorCodes.go
+++ configurations/errorCodes.go
@@ -59,6 +59,65 @@ const (
 	// Can not initialize postgres DB
 	ERROR_CAN_T_INIT_POSTGRES_DB
 
+	// Can not open or create http access log file
+	ERROR_CAN_T_OPEN_OR_CREATE_HTTP_ACCESS_LOG_FILE
+
+	// Can not open or create http error log file
+	ERROR_CAN_T_OPEN_OR_CREATE_HTTP_ERROR_LOG_FILE
+
+	// Can not listen & serve HTTP server
+	ERROR_CAN_T_LISTEN_AND_SERVE_HTTP_SERVER
+
+	// Can not stop api HTTP server
+	ERROR_CAN_T_STOP_API_HTTP_SERVER
+
+	// Can not insert device info
+	ERROR_CAN_T_INSERT_DEVICE_INFO
+
+	// Can not update device info
+	ERROR_CAN_T_UPDATE_DEVICE_INFO
+
+	// Can not insert metrics to postgres DB
+	ERROR_CAN_T_INSERT_METRICS_TO_POSTGRES_DB
+
+	// No metrics in the array
+	ERROR_NO_METRICS_IN_THE_ARRAY
+
+	// Can not insert version info
+	ERROR_CAN_T_INSERT_VERSION_INFO
+	// Can not update version info
+	ERROR_CAN_T_UPDATE_VERSION_INFO
+
+	// Can not insert protocol info
+	ERROR_CAN_T_INSERT_PROTOCOL_INFO
+	// Can not update protocol info
+	ERROR_CAN_T_UPDATE_PROTOCOL_INFO
+
+	// Can not insert autonomous system info
+	ERROR_CAN_T_INSERT_AUTONOMOUS_INFO
+	// Can not update autonomous system info
+	ERROR_CAN_T_UPDATE_AUTONOMOUS_INFO
+
+	// Can not insert host info
+	ERROR_CAN_T_INSERT_HOST_INFO
+	// Can not update host info
+	ERROR_CAN_T_UPDATE_HOST_INFO
+
+	// Can not insert port info
+	ERROR_CAN_T_INSERT_PORT_INFO
+	// Can not update port info
+	ERROR_CAN_T_UPDATE_PORT_INFO
+
+	// Can not insert GEO info
+	ERROR_CAN_T_INSERT_GEO_INFO
+	// Can not update GEO info
+	ERROR_CAN_T_UPDATE_GEO_INFO
+
+	// Can not insert flag info
+	ERROR_CAN_T_INSERT_FLAG_INFO
+	// Can not update flag info
+	ERROR_CAN_T_UPDATE_FLAG_INFO
+
 	// UNKOWN ERORR
 	ERROR_UKNOWN
 )
@@ -92,6 +151,38 @@ func (e ErrorCodes) String() string {
 
 		"Can not connect to Postgres DB",
 		"Can not initialize postgres DB",
+
+		"Can not open or create http access log file",
+		"Can not open or create http error log file",
+
+		"Can not listen & serve API HTTP server",
+		"Can not stop API HTTP server",
+
+		"Can not insert device info",
+		"Can not update device info",
+		"Can not insert metrics to postgres DB",
+		"No metrics in the array",
+
+		"Can not insert version info",
+		"Can not update version info",
+
+		"Can not insert protocol info",
+		"Can not update protocol info",
+
+		"Can not insert autonomous system info",
+		"Can not update autonomous system info",
+
+		"Can not insert host info",
+		"Can not update host info",
+
+		"Can not insert port info",
+		"Can not update port info",
+
+		"Can not insert geo info",
+		"Can not update geo info",
+
+		"Can not insert flag info",
+		"Can not update flag info",
 
 		"Unknown error",
 	}[e]
