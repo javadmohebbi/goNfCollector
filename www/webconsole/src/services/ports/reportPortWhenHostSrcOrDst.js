3ec3091135b980c6244ade3c118c2422c91fe4a9
M. Javad Mohebbi <mohammadjavadmohebbi@gmail.com>
2022-06-18 11:21:12 +0430

    get top src dst port by host

---
+++ www/webconsole/src/services/ports/reportPortWhenHostSrcOrDst.js
@@ -0,0 +1,24 @@
+import { UtlHttpRequest } from '../utils';
+
+
+export const ReportPortWheneHostSrcOrDst = ({
+    host = 'uknown',
+    top = 15,
+    direction = 'src',
+    interval = '24m',
+
+    signal
+}) => {
+    // AIP URL
+    const url = `${process.env.REACT_APP_HTTP}/port/report/${host}/as/${direction}/top/${top}/interval/${interval}`;
+
+    // send request to server
+    return UtlHttpRequest(
+        url,
+        'GET',
+        false,
+        false,
+        false,
+        signal
+    );
+};
